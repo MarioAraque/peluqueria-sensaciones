0c957d0e4de5444a7b15f80dd38a957330eb7b10
Mario Araque <mario@chicisimo.com>
2014-07-15 22:41:38 +0200

    Add files

---
+++ assets/javascript/lib/jquery.smint.js
@@ -0,0 +1,154 @@
+/*
+
+SMINT V1.0 by Robert McCracken
+SMINT V2.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy ‏(@mcpacosy)
+SMINT V3.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy ‏(@mcpacosy)
+
+SMINT is my first dabble into jQuery plugins!
+
+http://www.outyear.co.uk/smint/
+
+If you like Smint, or have suggestions on how it could be improved, send me a tweet @rabmyself
+
+*/
+
+
+(function(){
+
+
+	$.fn.smint = function( options ) {
+
+		var settings = $.extend({
+			'scrollSpeed'  : 500,
+			'mySelector'     : 'div'
+		}, options);
+
+		// adding a class to users div
+		$(this).addClass('smint');
+
+
+				
+		
+		//Set the variables needed
+		var optionLocs = new Array(),
+			lastScrollTop = 0,
+			menuHeight = $(".smint").height(),
+			smint = $('.smint'),
+        	smintA = $('.smint a'),
+        	myOffset = 0;
+
+      
+
+
+
+		if ( settings.scrollSpeed ) {
+				var scrollSpeed = settings.scrollSpeed
+			}
+
+		if ( settings.mySelector ) {
+				var mySelector = settings.mySelector
+		};
+
+
+
+		return smintA.each( function(index) {
+            
+			var id = $(this).attr('href').split('#')[1];
+
+			if (!$(this).hasClass("extLink")) {
+				$(this).attr('id', id);
+			}
+
+			
+			//Fill the menu
+			optionLocs.push(Array(
+				$(mySelector+"."+id).position().top-menuHeight, 
+				$(mySelector+"."+id).height()+$(mySelector+"."+id).position().top, id)
+			);
+
+			///////////////////////////////////
+
+			// get initial top offset for the menu 
+			var stickyTop = smint.offset().top;	
+
+			// run function every time you scroll
+			$(window).scroll(function() {
+				//Get the direction of scroll
+				var st = $(this).scrollTop()+myOffset;
+				if (st > lastScrollTop) {
+				    direction = "down";
+				} else if (st < lastScrollTop ){
+				    direction = "up";
+				}
+				lastScrollTop = st;
+
+				// Check if at bottom of page, if so, add class to last <a> as sometimes the last div
+				// isnt long enough to scroll to the top of the page and trigger the active state.
+
+				if($(window).scrollTop() + $(window).height() == $(document).height()) {
+	       			smintA.removeClass('active')
+	       			$(".smint a:not('.extLink'):last").addClass('active')
+	       			
+   				} else {
+   					smintA.last().removeClass('active')
+   				}
+			});
+
+			///////////////////////////////////////
+        
+        	$(this).on('click', function(e){
+				// gets the height of the users div. This is used for off-setting the scroll so the menu doesnt overlap any content in the div they jst scrolled to
+				var myOffset = 0;   
+
+        		// stops hrefs making the page jump when clicked
+				e.preventDefault();
+				
+				// get the hash of the button you just clicked
+				var hash = $(this).attr('href').split('#')[1];
+
+				
+
+				var goTo =  $(mySelector+'.'+ hash).offset().top-myOffset;
+				
+				// Scroll the page to the desired position!
+				$("html, body").stop().animate({ scrollTop: goTo }, scrollSpeed);
+				
+				// if the link has the '.extLink' class it will be ignored 
+		 		// Courtesy of mcpacosy ‏(@mcpacosy)
+				if ($(this).hasClass("extLink"))
+                {
+                    return false;
+                }
+
+			});	
+
+
+			//This lets yo use links in body text to scroll. Just add the class 'intLink' to your button and it will scroll
+
+			$('.intLink').on('click', function(e){
+				var myOffset = 0;   
+
+				e.preventDefault();
+				
+				var hash = $(this).attr('href').split('#')[1];
+
+				if (smint.hasClass('fxd')) {
+					var goTo =  $(mySelector+'.'+ hash).position().top-myOffset;
+				} else {
+					var goTo =  $(mySelector+'.'+ hash).position().top-myOffset*2;
+				}
+				
+				$("html, body").stop().animate({ scrollTop: goTo }, scrollSpeed);
+
+				if ($(this).hasClass("extLink"))
+                {
+                    return false;
+                }
+
+			});	
+		});
+
+	};
+
+	$.fn.smint.defaults = { 'scrollSpeed': 500, 'mySelector': 'div'};
+})(jQuery);
